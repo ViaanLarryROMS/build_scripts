abdb92e021bb26ec1dc3a1a57037e57c9618be05
ViaanROMS <viaan152013@gmail.com>
2026-01-25 18:50:06 +0530

    Create yaaptree.sh

---
+++ yaaptree.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/ViaanLarryROMS/android_device_oneplus_larry -b YAAP device/oneplus/larry
+git clone https://github.com/ViaanLarryROMS/android_device_oneplus_sm6375-common -b YAAP device/oneplus/sm6375-common
+git clone https://github.com/Teamhackneyed/proprietary_vendor_oneplus_larry -b lineage-23.2 vendor/oneplus/larry
+git clone https://github.com/Teamhackneyed/proprietary_vendor_oneplus_sm6375-common -b lineage-23.2 vendor/oneplus/sm6375-common
+git clone https://github.com/Teamhackneyed/android_kernel_oneplus_sm6375 -b lineage-23.2 kernel/oneplus/sm6375
+git clone https://github.com/ViaanLarryROMS/android_hardware_oplus -b YAAP hardware/oplus
